dae11d47e457647db999cc99576f792959795226
Subbhulakshman Prabusankar <ninjacoder17@gmail.com>
2026-03-04 16:18:06 +0530

    Add finished skeleton and R - Type

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -45,16 +45,41 @@ def main():
             if labIns == "":
                 continue
             ins = labIns
-        binNum = toBin(ins, pc, label)
+        binNum = toBin(ins, pc, labels)
         if binNum:
             output.append(binNum)
             pc += 4
 
+    # rishabh implement the code to write the binary string to the outputPath file.
 
+def toBin(instruction, pc, labels):
+    splitted = instruction.replace(",", " ").split()
+    operation = splitted[0]
 
-def toBin(instruction, pc, label):
-    return 1
+    RType = {
+        'add': ('0000000', '000'),
+        'sub': ('0100000', '000'),
+        'sll': ('0000000', '001'),
+        'slt': ('0000000', '010'),
+        'sltu': ('0000000', '011'),
+        'xor': ('0000000', '100'),
+        'srl': ('0000000', '101'),
+        'or': ('0000000', '110'),
+        'and': ('0000000', '111'),
+    }
 
+    if operation in RType:
+        rd = registers[splitted[1]]
+        rs1 = registers[splitted[2]]
+        rs2 = registers[splitted[3]]
+        funct7, funct3 = RType[operation]
+        return funct7 + rs2 + rs1 + funct3 + rd + "0110011"
+
+    # nishaanth add the i and s type here (refer to the r type for variables and format)
+    # nithilan add b and j type here
+    # rishabh add u type here
+
+    
 
 
 
