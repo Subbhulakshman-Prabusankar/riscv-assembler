ace8077a59c4912981a12f9cabd763c1fa9ad9fa
Subbhulakshman Prabusankar <ninjacoder17@gmail.com>
2026-03-04 15:34:05 +0530

    Add output code

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -32,21 +32,28 @@ def main():
         else:
             pc += 4
 
-    
+    # rishabh build an error checking function and run the line through them and print response or proceed further.
+
+    output = ""
+    pc = 0
+    for line in lines:
+        line = line.strip()
+        if line == " ":
+            continue
+        if ":" in line:
+            labIns = line.split(":", 1)[1].strip()
+            if labIns == "":
+                continue
+            ins = labIns
+        binNum = toBin(ins, pc, label)
+        if binNum:
+            output.append(binNum)
+            pc += 4
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+def toBin(instruction, pc, label):
+    return 1
 
 
 
